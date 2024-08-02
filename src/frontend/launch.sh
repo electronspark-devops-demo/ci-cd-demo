8cf72c11f26c63b9aacbe97acda7a4ef35ab7b0d
ElectronSpark <electronsparkle@gmail.com>
2024-08-02 01:24:54 -0400

    frontend launch script

---
+++ src/frontend/launch.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+envsubst '\$FRONTEND_DOMAIN \$BACKEND_DOMAIN' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '\$BACKEND_DOMAIN' < /usr/share/nginx/html/config.template.js > /usr/share/nginx/html/config.js
+exec nginx -g 'daemon off;'
