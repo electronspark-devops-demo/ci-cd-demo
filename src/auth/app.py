b7b53b9a228845acdd189e701755c3005382d1f0
ElectronSpark <electronsparkle@gmail.com>
2024-08-01 21:06:18 -0400

    app context

--- src/auth/app.py
+++ src/auth/app.py
@@ -86,5 +86,6 @@ def update_user():
     return jsonify({'message': 'user updated'})
 
 if __name__ == '__main__':
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=5001)
