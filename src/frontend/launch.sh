a364cb4ee8a5bbcd8cbbd1b047e61e1c9b21adc5
ElectronSpark <electronsparkle@gmail.com>
2024-08-04 15:33:54 -0400

    delete trash code

--- src/frontend/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-envsubst '\$FRONTEND_DOMAIN \$BACKEND_DOMAIN' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
-envsubst '\$BACKEND_DOMAIN' < /usr/share/nginx/html/config.template.js > /usr/share/nginx/html/config.js
-# exec nginx -g 'daemon off;'
-
-EXPOSE 5000
-CMD ["python", "app.py"]
