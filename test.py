5710280b7837c9022b652c05ef75d91e98448264
ElectronSpark <electronsparkle@gmail.com>
2024-08-06 01:26:41 -0400

    test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from src.frontend.app import get_index_title
 
 class TestHelloApp(unittest.TestCase):
 
-  def get_index_title(self):
+  def test_demo(self):
     self.assertEqual(get_index_title(), "Demo Blog Website")
 
 if __name__ == '__main__':
